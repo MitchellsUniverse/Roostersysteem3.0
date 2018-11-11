511ad0497f91c9322ba85ad37a10f7d540a05650
Micha Rijsmus <1759213rijsmus@zuyd.nl>
2018-11-11 19:24:19 +0100

    Final Version

--- Roostersysteem/Controllers/RoosterController.cs
+++ Roostersysteem/Controllers/RoosterController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using Roostersysteem.Models;
 
 namespace Roostersysteem.Controllers
@@ -14,118 +17,24 @@ namespace Roostersysteem.Controllers
     {
         private RoosterDB db = new RoosterDB();
 
-        // GET: Rooster
+        [HttpGet]
         public ActionResult Index()
         {
-            var roosters = db.Roosters.Include(r => r.PersoonVak).Include(r => r.VakLokaalType).Include(r => r.VakUrenCollege);
-            return View(roosters.ToList());
-        }
+            DataSet ds = new DataSet();
+            string constring = "Data Source=Micha-PC\\SQLEXPRESS;Initial Catalog=RoosterDB;Integrated Security=True";
 
-        // GET: Rooster/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
+            using (SqlConnection con = new SqlConnection(constring))
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                using (SqlCommand cmd = new SqlCommand("Select_Rooster", con))
+                {
+                    cmd.Connection = con;
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        sda.Fill(ds);
+                    }
+                }
             }
-            Rooster rooster = db.Roosters.Find(id);
-            if (rooster == null)
-            {
-                return HttpNotFound();
-            }
-            return View(rooster);
-        }
-
-        // GET: Rooster/Create
-        public ActionResult Create()
-        {
-            ViewBag.PersoonVak_Id = new SelectList(db.PersoonVaks, "PersoonVak_Id", "PersoonVak_Id");
-            ViewBag.VakLokaalType_Id = new SelectList(db.VakLokaalTypes, "VakLokaalType_Id", "VakLokaalType_Id");
-            ViewBag.VakUrenCollege_Id = new SelectList(db.VakUrenColleges, "VakUrenCollege_Id", "VakUrenCollege_Id");
-            return View();
-        }
-
-        // POST: Rooster/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "RoosterId,PersoonVak_Id,VakUrenCollege_Id,Klas,VakLokaalType_Id")] Rooster rooster)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Roosters.Add(rooster);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.PersoonVak_Id = new SelectList(db.PersoonVaks, "PersoonVak_Id", "PersoonVak_Id", rooster.PersoonVak_Id);
-            ViewBag.VakLokaalType_Id = new SelectList(db.VakLokaalTypes, "VakLokaalType_Id", "VakLokaalType_Id", rooster.VakLokaalType_Id);
-            ViewBag.VakUrenCollege_Id = new SelectList(db.VakUrenColleges, "VakUrenCollege_Id", "VakUrenCollege_Id", rooster.VakUrenCollege_Id);
-            return View(rooster);
-        }
-
-        // GET: Rooster/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Rooster rooster = db.Roosters.Find(id);
-            if (rooster == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.PersoonVak_Id = new SelectList(db.PersoonVaks, "PersoonVak_Id", "PersoonVak_Id", rooster.PersoonVak_Id);
-            ViewBag.VakLokaalType_Id = new SelectList(db.VakLokaalTypes, "VakLokaalType_Id", "VakLokaalType_Id", rooster.VakLokaalType_Id);
-            ViewBag.VakUrenCollege_Id = new SelectList(db.VakUrenColleges, "VakUrenCollege_Id", "VakUrenCollege_Id", rooster.VakUrenCollege_Id);
-            return View(rooster);
-        }
-
-        // POST: Rooster/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "RoosterId,PersoonVak_Id,VakUrenCollege_Id,Klas,VakLokaalType_Id")] Rooster rooster)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(rooster).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.PersoonVak_Id = new SelectList(db.PersoonVaks, "PersoonVak_Id", "PersoonVak_Id", rooster.PersoonVak_Id);
-            ViewBag.VakLokaalType_Id = new SelectList(db.VakLokaalTypes, "VakLokaalType_Id", "VakLokaalType_Id", rooster.VakLokaalType_Id);
-            ViewBag.VakUrenCollege_Id = new SelectList(db.VakUrenColleges, "VakUrenCollege_Id", "VakUrenCollege_Id", rooster.VakUrenCollege_Id);
-            return View(rooster);
-        }
-
-        // GET: Rooster/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Rooster rooster = db.Roosters.Find(id);
-            if (rooster == null)
-            {
-                return HttpNotFound();
-            }
-            return View(rooster);
-        }
-
-        // POST: Rooster/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Rooster rooster = db.Roosters.Find(id);
-            db.Roosters.Remove(rooster);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            return View(ds);
         }
 
         protected override void Dispose(bool disposing)
